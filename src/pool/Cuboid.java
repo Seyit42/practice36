ea39163e96c548677dbc140502acfeef46ff56f4
Ahmet Kaya <kayaseyitahmet42@gmail.com>
2020-03-28 19:06:48 +0000

    cuboid project is completed.

--- src/pool/Cuboid.java
+++ src/pool/Cuboid.java
@@ -1,9 +1,43 @@
 package pool;
 
-public class Cuboid {
+public class Cuboid extends Rectangle{
+	private double height;
+	
+//	Cuboid(double width, double length, double height){
+//		
+//	}
+	
+	Cuboid(double width, double length, double height) {
+		super(width, length);
+		this.height = height;
+		
+	}
 
 	public static void main(String[] args) {
+		Rectangle rectangle = new Rectangle(5, 10);
+		System.out.println("rectangle.width= " + rectangle.getWidth());
+		System.out.println("rectangle.length= " + rectangle.getLength());
+		System.out.println("rectangle.area= " + rectangle.getArea());
+		Cuboid cuboid = new Cuboid(5,10,5);
+		System.out.println("cuboid.width= " + cuboid.getWidth());
+		System.out.println("cuboid.length= " + cuboid.getLength());
+		System.out.println("cuboid.area= " + cuboid.getArea());
+		System.out.println("cuboid.height= " + cuboid.getHeight());
+		System.out.println("cuboid.volume= " + cuboid.getVolume());
 		
 	}
 	
+	public double getVolume() {
+		return height * getArea();
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	
 }
+
--- src/pool/Rectangle.java
+++ src/pool/Rectangle.java
@@ -1,5 +1,36 @@
 package pool;
 
 public class Rectangle {
+	private double width;
+	private double length;
+	
+	Rectangle(double width, double length){
+		this.setWidth(width);
+		this.setLength(length);
+	}
 
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		if (width < 0) {
+			width = 0;
+		}
+		this.width = width;
+	}
+
+	public double getLength() {
+		return length;
+	}
+
+	public void setLength(double length) {
+		if(length < 0) {
+			length = 0;
+		}
+		this.length = length;
+	}
+	public double getArea() {
+		return width * length;
+	}
 }
